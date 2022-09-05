26556c4dad7f73dd7e46f3b1397f4204ee5935f5
pooria89 <pooria.maleki89@gmail.com>
2022-09-05 17:10:16 +0430

    rename packageName .

--- app/src/main/java/com/image/MainActivity.kt
+++ app/src/main/java/com/image/MainActivity.kt
@@ -1,10 +1,10 @@
-package com.instagram
+package com.image
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.image.imageeditor.R
 import com.instagram.imageeditor.ImageEditorActivity
-import com.instagram.imageeditor.R
 
 
 class MainActivity : AppCompatActivity() {
